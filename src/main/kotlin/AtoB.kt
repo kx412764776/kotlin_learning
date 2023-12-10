ff7e27199125770d61b6915b02c7a537490d7c3b
QIZHAO CHEN <2766657C@student.gla.ac.uk>
2023-12-10 15:13:35 +0000

    kotlin convert data type & if expression

---
+++ src/main/kotlin/AtoB.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val number = "10"
+    println(10 + number.toInt())
+    println(10.toString() + number)
+    println()
+
+    val d = 3.0
+    println(d)
+    println(d.toInt())
+    println(d.toFloat())
+    println(d.toBigDecimal())
+    println(d.toUInt())
+    println(d.toRawBits())
+}
---
+++ src/main/kotlin/IfExpression.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val n1 = 21
+    val n2 = 20
+
+    var value = if (n1 > n2) {
+        println("True")
+        ":)"
+    } else {
+        println("False")
+        ":("
+    }
+
+    println(value)
+    println("Some code")
+}
