870f7c26262d4ea794450e7fb41ce1dfcbc356bd
QIZHAO CHEN <2766657C@student.gla.ac.uk>
2023-12-10 01:16:33 +0000

    kotlin data types & string

--- src/main/kotlin/DataTypes.kt
+++ src/main/kotlin/DataTypes.kt
@@ -7,7 +7,24 @@ fun main() {
     var b: Boolean = true
     var s: String = "kotlin"
     var c: Char = 'a'
+
     // don't suggest use any
     var x: Any = "13"
     x = 13
+
+    var brandVar = "kotlin"
+    brandVar = "KOTLIN"
+
+    // Read only.
+    // final String brand = "kotlin";
+    val brandVal = "kotlin"
+
+    // null safety
+    var brand1 = "end"
+    println(brand1.uppercase())
+
+    var brand2: String? = null
+    println(brand2?.uppercase())
+
 }
+const val PI = 3.14 // static final
---
+++ src/main/kotlin/Strings.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val name = "Ahmed"
+    println(name)
+    println(name.length)
+    println(name.uppercase())
+    println(name.lowercase())
+    println(name[0])
+    println(name[4])
+    println(name.isEmpty())
+    println("".isEmpty())
+
+    val age = 20
+
+    // string templates
+    // val msg = "${name} ${age}"
+     val msg = "Name: $name Age: $age"
+    println(msg)
+}
